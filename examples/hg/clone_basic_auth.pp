25bcaf4d115717c8278ab997851873528461708b
Stuart Whelan <stuart@somepointinthefuture.co.nz>
2014-05-06 11:09:38 +1200

    Added basic_auth to hg
    Added support for basic authentication to hg provider
    Updated unit tests
    Updated hg readme and added examples

---
+++ examples/hg/clone_basic_auth.pp
@@ -0,0 +1,8 @@
+vcsrepo { '/tmp/vcstest-hg-clone':
+  ensure   => present,
+  provider => hg,
+  source   => 'http://hg.basho.com/riak',
+  revision => 'riak-0.5.3',
+  basic_auth_username	=> 'username',
+  basic_auth_password	=> 'eightstars',
+}
