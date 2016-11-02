f4dc5885386eeb1382641910afb3bb2e8a7978c1
Wilson McCoubrey <wilson@puppet.com>
2016-11-02 12:39:02 +0000

    Add a custom fact for svn client version. Disable the SVN tests which utilize the \'includes\' when SVN version < 1.7 and update the README note that /'includes/' option is not supported where SVN < 1.7.

--- lib/puppet/provider/vcsrepo/svn.rb
+++ lib/puppet/provider/vcsrepo/svn.rb
@@ -220,6 +220,12 @@ Puppet::Type.type(:vcsrepo).provide(:svn, :parent => Puppet::Provider::Vcsrepo) 
   end
 
   def update_includes(paths)
+    #If svn version < 1.7, '--parents' isn't supported. Raise legible error.
+    svn_ver = get_svn_client_version
+    if svn_ver == nil || Gem::Version.new(svn_ver) < Gem::Version.new('1.7')
+      raise "Includes option is not available for SVN versions < 1.7. Version installed: #{svn_ver}"
+    end
+
     at_path do
       args = buildargs.push('update')
       if @resource.value(:revision)
@@ -232,6 +238,12 @@ Puppet::Type.type(:vcsrepo).provide(:svn, :parent => Puppet::Provider::Vcsrepo) 
       args.push(*paths)
       svn(*args)
     end
+  end
+
+  def get_svn_client_version
+    ver = Facter.value(:vcsrepo_svn_version)
+    return '0.0' if ver == nil
+    return ver;
   end
 
 end
