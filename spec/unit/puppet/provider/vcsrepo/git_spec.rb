9c6bf6808015abe48a0ba945e6062fb6bb0a6d14
Dmytro Makovey <dmakovey@stanford.edu>
2017-05-05 16:14:32 -0700

    fixing force parameter to be boolean remove test that can never succeed

--- spec/unit/puppet/provider/vcsrepo/git_spec.rb
+++ spec/unit/puppet/provider/vcsrepo/git_spec.rb
@@ -223,7 +223,7 @@ branches
         provider.expects(:path_exists?).returns(true)
         provider.expects(:path_empty?).returns(false)
         ## this test can never succeed due to logic in
-        ## create/check_force
+        ##  create/check_force
         # provider.expects(:working_copy_exists?).returns(false)
         expect { provider.create }.to raise_error(Puppet::Error)
       end
