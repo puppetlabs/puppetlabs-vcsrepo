6c8420097f828c5c0b4d531c077467f96807caf8
Dmytro Makovey <dmakovey@stanford.edu>
2017-05-09 14:56:04 -0700

    the entire test suite is geared for current behavior of the code sans one test. leaving it disabled, moving on.

--- lib/puppet/provider/vcsrepo/git.rb
+++ lib/puppet/provider/vcsrepo/git.rb
@@ -11,11 +11,7 @@ Puppet::Type.type(:vcsrepo).provide(:git, :parent => Puppet::Provider::Vcsrepo) 
     :user, :depth, :branch, :submodules
 
   def create
-    if path_exists? and not path_empty? and not working_copy_exists?
-      raise Puppet::Error, "Path %s exists and is not the desired repository." % @resource.value(:path)
-    else
-      check_force
-    end
+    check_force
     if @resource.value(:revision) and ensure_bare_or_mirror?
       fail("Cannot set a revision (#{@resource.value(:revision)}) on a bare repository")
     end
