e386c309390619e044030192e2e2610dc11f012f
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-04-29 09:43:53 +0100

    Remove unneeded variable

--- lib/puppet/provider/vcsrepo/hg.rb
+++ lib/puppet/provider/vcsrepo/hg.rb
@@ -129,13 +129,11 @@ Puppet::Type.type(:vcsrepo).provide(:hg, parent: Puppet::Provider::Vcsrepo) do
       options.merge!(args.pop)
     end
 
-    sensitive = sensitive?
-
     if @resource.value(:basic_auth_username) && @resource.value(:basic_auth_password)
       args += [
         '--config', "auth.x.prefix=#{@resource.value(:source)}",
         '--config', "auth.x.username=#{@resource.value(:basic_auth_username)}",
-        '--config', "auth.x.password=#{sensitive ? @resource.value(:basic_auth_password).unwrap : @resource.value(:basic_auth_password)}",
+        '--config', "auth.x.password=#{sensitive? ? @resource.value(:basic_auth_password).unwrap : @resource.value(:basic_auth_password)}",
         '--config', 'auth.x.schemes=http https'
       ]
     end
@@ -147,9 +145,9 @@ Puppet::Type.type(:vcsrepo).provide(:hg, parent: Puppet::Provider::Vcsrepo) do
     args.map! { |a| (a =~ %r{\s}) ? "'#{a}'" : a } # Adds quotes to arguments with whitespaces.
 
     if @resource.value(:user) && @resource.value(:user) != Facter['id'].value
-      Puppet::Util::Execution.execute("hg #{args.join(' ')}", uid: @resource.value(:user), failonfail: true, combine: true, sensitive: sensitive)
+      Puppet::Util::Execution.execute("hg #{args.join(' ')}", uid: @resource.value(:user), failonfail: true, combine: true, sensitive: sensitive?)
     else
-      Puppet::Util::Execution.execute("hg #{args.join(' ')}", sensitive: sensitive)
+      Puppet::Util::Execution.execute("hg #{args.join(' ')}", sensitive: sensitive?)
     end
   end
 end
