c3262cde2186b132c685b18501d49a307b3bd432
Ramesh Sencha <ramesh.sencha@perforce.com>
2023-11-29 10:18:17 +0530

    (CAT-1608) - PDK update

--- lib/puppet/provider/vcsrepo/dummy.rb
+++ lib/puppet/provider/vcsrepo/dummy.rb
@@ -11,8 +11,8 @@ Puppet::Type.type(:vcsrepo).provide(:dummy, parent: Puppet::Provider::Vcsrepo) d
   def working_copy_exists?
     providers = begin
       @resource.class.providers.map(&:to_s).sort.reject { |x| x == 'dummy' }.join(', ')
-    rescue StandardError
-      'none'
+                rescue StandardError
+                  'none'
     end
     raise("vcsrepo resource must have a provider, available: #{providers}")
   end
